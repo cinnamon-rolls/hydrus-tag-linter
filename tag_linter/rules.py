a98f7c6dbbc81e096b5aa23e53f509dca88727aa
cinnamon-rolls <>
2021-07-13 20:30:00 -0400

    mutual exclusion template

--- tag_linter/rules.py
+++ tag_linter/rules.py
@@ -30,18 +30,60 @@ def template_helicopter(data) -> List[Rule]:
 
     tags.extend([('-' + i) for i in children])
 
-    return [
-        construct_one({
-            'name': data.get('name', 'heli-parent ' + parent),
-            'note': data.get('note', "the tag '" + parent + "' should accompany one or more of its children"),
-            'search': tags,
-            'disabled': data.get('disabled', False)
-        })]
+    return construct_one({
+        'name': data.get('name', 'heli-parent ' + parent),
+        'note': data.get('note', "the tag '" + parent + "' should accompany one or more of its children"),
+        'search': tags,
+        'disabled': data.get('disabled', False)
+    })
+
+
+def template_mutual_exclusion(data) -> List[Rule]:
+    tags = data.get('tags')
+    if tags is None or len(tags) < 2:
+        return []
+
+    if len(tags) > 2:
+        raise ValueError('for now, only lists of length 2 are supported')
+
+    defaultName = "mutual exclusion: " + ", ".join(tags)
+
+    def create_only_action(tag):
+        return {
+            'name': "Only '" + tag + "'",
+            'archetype': 'change_tags',
+            'shortcut': 'auto',
+            'resolves': True,
+            'hints': {
+                'add_tags': [tag],
+                'rm_tags': [i for i in tags if i is not tag]
+            }
+        }
+
+    actions = [create_only_action(i) for i in tags]
+
+    actions.append({
+        'name': "None (Remove all)",
+        'archetype': 'change_tags',
+        'shortcut': 'auto',
+        'resolves': True,
+        'hints': {
+            'rm_tags': tags
+        }
+    })
+
+    return template_default({
+        'name': data.get('name', defaultName),
+        'note': "The tags " + ", ".join(tags) + " are mutually exclusive, so only one should be present",
+        'search': tags,
+        'actions': actions
+    })
 
 
 templates = {
     'default': template_default,
-    'helicopter parent': template_helicopter
+    'helicopter parent': template_helicopter,
+    'mutual exclusion': template_mutual_exclusion
 }
 
 
@@ -55,7 +97,7 @@ def apply_template(data: dict, template_name: Union[str, None] = None) -> List[R
     return template(data)
 
 
-def load_rule_from_data(data: Union[None, Dict, List]) -> List[Rule]:
+def load_rules_from_data(data: Union[None, Dict, List]) -> List[Rule]:
     """
     Reads a rule from a some object (likely obtained from an already parsed JSON
     document), and returns all of the rules that could be parsed from it.
@@ -67,11 +109,14 @@ def load_rule_from_data(data: Union[None, Dict, List]) -> List[Rule]:
     if isinstance(data, list):
         ret = []
         for i in data:
-            ret.extend(load_rule_from_data(i))
+            ret.extend(load_rules_from_data(i))
         return ret
 
     if isinstance(data, dict):
-        return apply_template(data=data)
+        ret = apply_template(data=data)
+        if not isinstance(ret, Iterable):
+            ret = [ret]
+        return ret
 
     raise ValueError(
         'Unsure how to parse Rule from type ' +
@@ -93,7 +138,7 @@ def load_rules_from_file(rule_file_name: str) -> List[Rule]:
     with open(rule_file_name) as rule_file:
         data = json.load(rule_file)
 
-    return load_rule_from_data(data=data)
+    return load_rules_from_data(data=data)
 
 
 def load_rules_from_dirs(rules_dirs: Union[str, List[str]]) -> List[Rule]:
@@ -152,6 +197,7 @@ def load_rules(paths: Union[str, Iterable[str]]) -> Dict[str, Rule]:
         else:
             rules = load_rules_from_file(path)
 
+        print(rules)
         for rule in rules:
             ret[rule.get_name().strip().lower()] = rule
 
