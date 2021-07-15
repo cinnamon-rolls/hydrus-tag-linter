8aecd3fd405e65dd04802dd5585892e9f946a07b
cinnamon-rolls <>
2021-07-15 13:20:07 -0400

    add summary to home

--- react-app/src/components/pageHome.jsx
+++ react-app/src/components/pageHome.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 
 class PageHome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.refreshSummary();
+  }
+
+  getSummary() {
+    return this.state.summary || [];
+  }
+
+  async refreshSummary() {
+    return fetch("/api/server/get_summary")
+      .then((resp) => resp.json())
+      .then((summary) => this.setState({ summary }));
+  }
+
   render() {
-    return <span>WIP (Home)</span>;
+    return (
+      <div>
+        <h2>Summary</h2>
+        {this.getSummary().map((section) => (
+          <div>
+            <h3>{section.name}</h3>
+            <table>
+              {section.value.map((data) => (
+                <tr>
+                  <td>{data.name}</td>
+                  <td>
+                    <code>{data.value + ""}</code>
+                  </td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
