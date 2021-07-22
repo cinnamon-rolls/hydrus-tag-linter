da427a7d1753c2f753de670c61a1e12a381bade3
cinnamon-rolls <>
2021-07-22 11:13:08 -0400

    update example scripts

---
+++ example_run.bat
@@ -0,0 +1,5 @@
+:: Note: I do not use Windows regularly. This script was contributed by a different user.
+:: The following command should be pasted in to the Command Prompt (CMD)
+:: PowerShell may or may not work
+
+py.exe -3 __main__.py --api_url "http://127.0.0.1:45870/"  -H "localhost" -P "45868" -r "my-rules" "default-rules" -k "[API Key Here]"
